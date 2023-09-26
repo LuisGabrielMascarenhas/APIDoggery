4a55a1dde1d6280dee23a9925c4a8936828a0831
LuisGabrielMascarenhas <luis.gabriel.santos614@gmail.com>
2023-09-26 20:58:54 -0300

    operação do banco

--- includes/DBOperation.php
+++ includes/DBOperation.php
@@ -6,6 +6,11 @@ class DBOperation{
 
     function __construct(){
         require_once dirname(__FILE__) .'DBconnection.php';
+
+
+        $db = new DBconnect();
+
+        $this->con =$db->connect();
     }
 
 }
